e0d7f3bd8f1ecc660421e16651dfe829b8193591
ThiagoAraujoMoutinho <119462257+ThiagoAraujoMoutinho@users.noreply.github.com>
2023-11-23 20:52:20 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# aula87
+# PRO-C87-Ativ-Aluno1-1_m-Solucao
---
+++ screens/SpaceCraft.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+import {Text, View} from "react-native";
+
+export default class SpaceCraftScreen extends Component{
+    render(){
+        return(
+            <View style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center"
+            }}>
+                <Text>Tela de nave espacial</Text>
+            </View>
+        )
+    }
+}
